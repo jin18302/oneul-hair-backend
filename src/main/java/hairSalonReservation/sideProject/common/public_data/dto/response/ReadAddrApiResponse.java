5e25698091962d4be688fb8352e6a5ef9574331c
jin18302 <jin18302@naver.com>
2026-01-02 04:23:05 +0900

    refactor: ReadAddrApiResponse 기본값 세팅

--- src/main/java/hairSalonReservation/sideProject/common/public_data/dto/response/ReadAddrApiResponse.java
+++ src/main/java/hairSalonReservation/sideProject/common/public_data/dto/response/ReadAddrApiResponse.java
@@ -8,8 +8,13 @@ public record ReadAddrApiResponse(List<ReadAddrResponse> result) {
     public ReadAddrApiResponse(List<ReadAddrResponse> result){
 
         List<ReadAddrResponse> resultList = new ArrayList<>();
-        resultList.add(0, ReadAddrResponse.from("0", "전체", "전체"));
-        resultList.addAll(result);
-       this.result = resultList;
+
+        if(result != null){
+            resultList.add(0, ReadAddrResponse.from("0", "전체", "전체"));
+            //이거를 해당 지역의 값으로 넣으면 되잖아 0대산에
+            resultList.addAll(result);
+        }
+
+        this.result = resultList;
     }
 }
